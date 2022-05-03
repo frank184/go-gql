29288e4916f2e4359a6172b3a3622eafb15d8eed
Frank <francoisbelanger1993@hotmail.com>
2022-05-03 10:18:05 -0400

    init

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,28 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+	"go-gql/v1/graph/generated"
+	"go-gql/v1/graph/model"
+)
+
+func (r *mutationResolver) UpsertUser(ctx context.Context, input *model.UserInput) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
